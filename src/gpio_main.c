f65faef820e24fc4550a77e175ac6c2649d175ef
AndreaRantin2301 <andrea.rantin.01.23@gmail.com>
2025-05-06 17:02:17 +0200

    Added gpio_driver with mode set, pin write,read,toggle, set alternate function. Added unit tests

---
+++ src/gpio_main.c
@@ -0,0 +1,14 @@
+/*
+    This file is for internal testing purposes only
+    remove this every time you merge to avoid conflicts with the
+    actual main
+*/
+
+int main(void) {
+
+    while(1) {
+
+    }
+
+    return 0;
+}
