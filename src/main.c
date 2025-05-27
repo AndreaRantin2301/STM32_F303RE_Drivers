4c48df2bdf98520c6482acd98060e17c6b544778
AndreaRantin2301 <andrea.rantin.01.23@gmail.com>
2025-05-27 16:15:17 +0200

    Added some test code

--- src/main.c
+++ src/main.c
@@ -5,20 +5,33 @@
 
 int main(void) {
 
+    ClockInitStruct clockInitStruct = {0};
+
+    clockInitStruct.source = CLOCK_SOURCE_PLL;
+    clockInitStruct.prediv = PREDIV_1;
+    clockInitStruct.pllSrc = PLL_SRC_HSI;
+    clockInitStruct.pllMul = PLL_MUL_9;
+    clockInitStruct.flashLatency = FLASH_LATENCY_TWO_WAIT;
+    clockInitStruct.ahbPre = AHB_PRE_NONE;
+    clockInitStruct.apb1Pre = APB1_PRE_2;
+    clockInitStruct.apb2Pre = APB2_PRE_NONE;
+
     //Initialize system clock
-    if(System_Clock_Init() != CLOCK_OK) {
+    if(System_Clock_Init(clockInitStruct) != CLOCK_OK) {
         while(1);
     }
 
     //Initialize systick with a 1ms trigger
     SysTick_Init();
 
-    //Enable clock on USER LED
-    if(GPIO_Clock_Enable(GPIO_PORT_A) != CLOCK_OK) {
-        while(1);
-    }
+    GPIO_Init_Struct gpioInitStruct = {0};
 
-    if(GPIO_Set_Mode(GPIO_PORT_A,GPIO_PIN_5,GPIO_MODE_OUTPUT) != GPIO_OK) {
+    gpioInitStruct.portEnum = GPIO_PORT_A;
+    gpioInitStruct.pinEnum = GPIO_PIN_5;
+    gpioInitStruct.mode  =GPIO_MODE_OUTPUT;
+    gpioInitStruct.af = GPIO_AF_NONE;
+
+    if(GPIO_Init(gpioInitStruct) != GPIO_OK) {
         while(1);
     }
 
